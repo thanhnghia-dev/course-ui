5cd0cae7eb9bd47453c91684bc0046d7ff122b6a
hothanhnghia-nlu <nghiant369@gmail.com>
2025-01-17 09:06:55 +0700

    Release web on production env.

--- src/service/CustomizeAxios.js
+++ src/service/CustomizeAxios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/v1/'
-    // baseURL: 'https://course-api-io3d.onrender.com/api/v1/'
+    baseURL: 'https://course-api-io3d.onrender.com/api/v1/'
 });
 
 // Add a response interceptor
