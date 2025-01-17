b63e643ad07d9f46f6fb8159e405204816c4c198
hothanhnghia-nlu <nghiant369@gmail.com>
2025-01-17 09:39:23 +0700

    Release web on production environment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,11 @@ import React from "react";
 import NavigationBar from "../components/Header";
 import Header from "../components/NavigationBar";
 import Statistical from "../components/Statistical";
+import {TabTitle} from "../utils/DynamicTitle";
 
 const Home = () => {
+    TabTitle('Trung Tâm Tin Học LP');
+
     return (
         <>
             <Header/>
