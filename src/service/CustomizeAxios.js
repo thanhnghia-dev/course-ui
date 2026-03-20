6d8ed1b363598ba0675323967bf3c49c08d14811
Hồ Thanh Nghĩa <129092341+thanhnghia-dev@users.noreply.github.com>
2026-03-20 09:43:42 +0700

    Configure baseUrl

--- src/service/CustomizeAxios.js
+++ src/service/CustomizeAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/v1/'
+    baseURL: 'https://course-server.up.railway.app/api/v1/'
 });
 
 // Add a response interceptor
